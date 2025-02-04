c511910a271aba2bf9d0f61b8a07ff6c19d6243d
Danielito ANDAG <danielito.andag@amadeus.com>
2025-02-04 09:20:52 +0800

    fix: added workers in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,30 +1,31 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './e2e/playwright',
-  fullyParallel: true,
-  retries: 2,
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.URL,
+	testDir: './e2e/playwright',
+	fullyParallel: true,
+	workers: 2,
+	retries: 2,
+	use: {
+		/* Base URL to use in actions like `await page.goto('/')`. */
+		baseURL: process.env.URL,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
-    screenshot: 'only-on-failure'
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
-    },
-    {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'webkit',
-      use: { browserName: 'webkit' },
-    },
-  ]
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: 'retain-on-failure',
+		video: 'retain-on-failure',
+		screenshot: 'only-on-failure',
+	},
+	projects: [
+		{
+			name: 'chromium',
+			use: { browserName: 'chromium' },
+		},
+		{
+			name: 'firefox',
+			use: { browserName: 'firefox' },
+		},
+		{
+			name: 'webkit',
+			use: { browserName: 'webkit' },
+		},
+	],
 });
