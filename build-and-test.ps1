39afc2b585a98b1c133db3589f3a0a39121c11ff
dcandag <danielcalebandag@gmail.com>
2025-09-25 17:12:18 +0800

    feat: add helper script

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -4,3 +4,9 @@ $env:URL = "http://localhost:4200/"
 npm install
 # Install playwright browser dependencies
 npx playwright install
+Write-Host "Starting the Angular application in the background..." -ForegroundColor Blue
+$job = Start-Job { ng serve }
+Write-Host "Angular application started with Job ID: $($job.Id)" -ForegroundColor Cyan
+npx playwright test e2e/playwright/tests
+Stop-Job $job.Id
+Write-Host "Angular application stopped." -ForegroundColor Green
